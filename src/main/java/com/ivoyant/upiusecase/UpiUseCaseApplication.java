ee4abb140512d9b9c2bb66693a945862caef42d9
Sagar-Patil-C94 <sagarp@ivoyant.com>
2024-02-21 12:19:23 +0530

    Initial Commit

---
+++ src/main/java/com/ivoyant/upiusecase/UpiUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.ivoyant.upiusecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiUseCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpiUseCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivoyant/upiusecase/UpiUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoyant.upiusecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiUseCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
