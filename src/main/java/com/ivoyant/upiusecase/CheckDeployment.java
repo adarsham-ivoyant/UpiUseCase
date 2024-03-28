c9ca711553afd5ef291065c561a8f0f11ca9a4d5
Sagar Patil <152495056+Sagar-Patil-C94@users.noreply.github.com>
2024-03-28 10:41:31 +0530

    Update Controller

--- src/main/java/com/ivoyant/upiusecase/CheckDeployment.java
+++ src/main/java/com/ivoyant/upiusecase/CheckDeployment.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckDeployment {
     @GetMapping("/deploycheck")
     public String checkDeployment() {
-        return "Hello World Test Deployment";
+        return "Hello World From CICD Deployment";
     }
 }
